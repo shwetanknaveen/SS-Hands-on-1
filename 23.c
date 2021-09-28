6738ec99b70f37efc1448c67df0a15ad5729555a
shwetanknaveen <39495857+shwetanknaveen@users.noreply.github.com>
2021-09-28 16:25:33 +0530

    Update 23.c

--- 23.c
+++ 23.c
@@ -9,6 +9,7 @@ int main()
 	{
 		while(1);//parent will no sleep and wait to get the child exit status neither parent is getting finished hence child
             //will finish execution but since its exit status won't be read by parent hence it will remain there as zombie
+		//parent reads child exit status at the end of its (parent) execution
             //  https://www.geeksforgeeks.org/zombie-and-orphan-processes-in-c/
 	} 
 	else if (pid == 0) 
